48dcb0e010f2be586c7d7e8a116ae85093d2ef60
FABIO RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2022-03-26 17:11:40 -0300

    Add files via upload

--- debian.sh
+++ debian.sh
@@ -1,22 +1,46 @@
-Por padrão, o sudo não está instalado no Debian, mas você pode instalá-lo. Primeiro ative o modo su:
+#!/usr/bin/env bash
+# -------------------------------------------------------
+# Licença : GNU/GPL v3.0
+# Script : [bspwm.sh]
+# Descrição :
+# Versão : 0.1
+# Autor : Fabio Junior Ribeiro <rib3iro@live.com>
+# Data : 08/12/2020
+# Licença : GNU/GPL v3.0
+# -------------------------------------------------------
+# Uso :
+# -------------------------------------------------------
+source variaveis.sh
+clear
+# Tela de boas vindas
+echo -e "${S} ${B}Bem vindo a instalação do bspwm${F}"
+sleep 1s
+clear
 
-$ su -
+# Por padrão, o sudo não está instalado no Debian, mas você pode instalá-lo. Primeiro ative o modo su:
 
-Instale o sudo executando:
+su -
 
-# apt-get install sudo -y
+# Instale o sudo executando:
 
-Depois disso, você precisaria brincar com usuários e permissões. Dê o sudo diretamente ao seu próprio usuário.
+apt-get install sudo -y
 
-# usermod -aG sudo yourusername
+# Depois disso, você precisaria brincar com usuários e permissões. Dê o sudo diretamente ao seu próprio usuário.
 
-Verifique se o arquivo sudoers foi adicionado ao grupo sudo. Execute:
-visudopara modificar o arquivo sudoers e adicione a seguinte linha (se estiver faltando):
+usermod -aG sudo yourusername
+
+# Verifique se o arquivo sudoers foi adicionado ao grupo sudo. Execute: visudo para modificar o arquivo sudoers e adicione a seguinte linha (se estiver faltando):
 
 # Allow members of group sudo to execute any command
 
 %sudo   ALL=(ALL:ALL) ALL
 
-Você precisa se conectar ou reiniciar completamente o dispositivo para que as alterações entrem em vigor.
+# Você precisa se conectar ou reiniciar completamente o dispositivo para que as alterações entrem em vigor.
 
-sudo apt install build-essential cmake 
+xdg-user-dirs xdg-utils
+
+xdg-user-dirs-update
+
+sudo apt install xorg build-essential cmake firefox-esr-l10n-pt-br xfce4-terminal xfonts-terminus rofi dunst -y
+
+sudo dpkg-reconfigure -plow console-setup
