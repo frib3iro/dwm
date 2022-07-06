bd98707447fa5f1a3003584fea5ca2ab4058b329
FABIO RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2022-07-05 23:53:04 -0300

    Add files via upload

--- dwm.sh
+++ dwm.sh
@@ -1,26 +1,42 @@
 #!/usr/bin/bash
 
 clear
+echo ">>> Criando pasta arquivos..."
+mkdir arquivos && cd arquivos
 
-echo
-echo ">>> Baixando dwm"
+clear
+echo ">>> Baixando dwm..."
 sleep 1s
 wget https://dl.suckless.org/dwm/dwm-6.3.tar.gz
 
-echo
-echo ">>> Baixando dmenu"
+clear
+echo ">>> Baixando dmenu..."
 sleep 1s
 wget https://dl.suckless.org/tools/dmenu-5.1.tar.gz
 
-echo
-echo ">>> Baixando st"
+clear
+echo ">>> Baixando st..."
 sleep 1s
 wget https://dl.suckless.org/st/st-0.8.5.tar.gz
 
-echo
-echo ">>> Descompactando arquivos"
+clear
+echo ">>> Descompactando arquivos..."
 sleep 1s
 for i in *.gz; do
     tar -zxvf $i
     rm -rf $i
 done
+
+clear
+echo "Criando o arquivo dwm.desktop..."
+sleep 1s
+cat > '/usr/share/xsessions/dwm.desktop' << EOF
+[Desktop Entry]
+Name=DWM
+Comment=DWM
+Exec=dwm
+Icon=dwm
+Type=XSession
+EOF
+
+
