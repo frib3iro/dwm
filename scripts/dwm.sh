8ed49145b2bff40f7f9a3e711ed937e5ab5c2ff4
FABIO RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2022-07-16 16:22:20 -0300

    Add files via upload

--- scripts/dwm.sh
+++ scripts/dwm.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
+#----------------------------------------------------------------------
+# Script    : [dwm.sh]
+# Descrição :
+# Versão    : 1.0
+# Autor     : Fabio Junior Ribeiro 
+# Email     : rib3iro@live.com
+# Github    : https://github.com/frib3iro 
+# Data      : 15/07/2022
+# Licença   : GNU/GPL v3.0
+#----------------------------------------------------------------------
+# Uso       :
+#----------------------------------------------------------------------
 
 clear
 
-# Lista de pacotes para instalar com o pacman
-listapacman=(aircrack-ng arc-gtk-theme archlinux-wallpaper arc-icon- bluez bluez-utils bully capitaine-cursors cmatrix cowpatty cronie cups fdupes feh ffmpegthumbnailer flameshot geany geany-plugins gst-libav gst-plugin-pipewire gufw hashcat hcxdumptool hcxtools htop libreoffice libreoffice-fresh-pt-br links lollypop lxappearance man-pages-pt_br mesa-demos mplayer neofetch picom ranger reaver rsync tcpdump termshark theme thunar ttf-droid ttf-font-awesome ttf-inconsolata ttf-opensans tumbler unrar wifite xclip xcursor-vanilla-dmz xcursor-vanilla-dmz-aa)
-
-echo "Instalando pacotes com pacman..."
-for i in ${listapacman[@]}; do
-    echo
-    echo "Instalando o pacote $i"
-    if pacman -S $i --noconfirm; then
-        echo "Pacote $i instalado com sucesso!"
-    else
-        echo "Houve erro na instalação do pacote $i!"
-    fi
-done
+echo "Criando diretorio para baixar arquivos..."
+mkdir ~/dwm && cd ~/dwm
 
 echo
-echo "Iniciando o bluez, cups lightdm..."
-systemctl enable bluetooth.service
-systemctl enable cups.service
+echo "Clonando dwm..."
+git clone https://git.suckless.org/dwm
 
 echo
-echo "Colorindo a saída do pacman..."
-sleep 1s
-sed -i 's/#Color/Color/' /etc/pacman.conf
+echo "Clonando dmenu..."
+git clone https://git.suckless.org/dmenu
+
+echo
+echo "Clonando st..."
+git clone https://git.suckless.org/st
 
